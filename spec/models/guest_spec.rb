efd7d1eff7f21da67e344caabb3f9d4335a1578e
Ed Salinas <edsalinas7b@yahoo.com>
2023-01-25 07:14:50 +0800

    Add tests for model associations

--- spec/models/guest_spec.rb
+++ spec/models/guest_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Guest, type: :model do
   it { should validate_uniqueness_of(:email) }
+
+  it 'has many reservations' do
+    expect(described_class.reflect_on_association(:reservations).macro).to eq :has_many
+  end
 end
--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Reservation, type: :model do
   it { should validate_uniqueness_of(:code) }
+
+  it 'belongs to guest' do
+    expect(described_class.reflect_on_association(:guest).macro).to eq :belongs_to
+  end
 end
