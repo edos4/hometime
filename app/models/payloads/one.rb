ba3dd623d1844165240b698689508c774b92f0ab
Ed Salinas <edsalinas7b@yahoo.com>
2023-01-25 17:59:45 +0800

    Create separate payloads

---
+++ app/models/payloads/one.rb
@@ -0,0 +1,37 @@
+class Payloads::One
+  def self.keys
+    [
+      "start_date","end_date", "nights", "guests", "adults", "children", "infants",
+      "status", "guest", "currency", "payout_price", "security_price", "total_price",
+      "controller", "action", "reservation_code"
+    ]
+  end
+
+  def self.create_reservation(reservation_params)
+    r = reservation_params
+    guest = Guest.find_or_create_by(id: r[:guest][:id])
+    guest.update(
+      email: r[:guest][:email],
+      first_name: r[:guest][:first_name],
+      last_name: r[:guest][:last_name],
+      phone_numbers:r[:guest][:phone]
+    )
+    Reservation.create!(
+      code: r[:reservation_code],
+      guest_id: guest.id,
+      start_date: r[:start_date],
+      end_date: r[:end_date],
+      nights: r[:nights],
+      number_of_adults: r[:adults],
+      number_of_children: r[:children],
+      number_of_infants: r[:infants],
+      status: r[:status],
+      currency: r[:currency],
+      payout_price: r[:payout_price],
+      security_price: r[:security_price],
+      total_price: r[:total_price]
+    )
+
+    guest
+  end 
+end
---
+++ app/models/payloads/two.rb
@@ -0,0 +1,35 @@
+class Payloads::Two
+  def self.keys
+    ["reservation", "controller", "action"]
+  end
+
+  def self.create_reservation(reservation_params)
+    r = reservation_params[:reservation]
+    guest = Guest.find_or_create_by(id: r[:guest_id])
+    guest.update(
+      email: r[:guest_email],
+      first_name: r[:guest_first_name],
+      last_name: r[:guest_last_name],
+      phone_numbers:r[:guest_phone_numbers]
+    )
+    Reservation.create!(
+      code: r[:code],
+      guest_id: guest.id,
+      start_date: r[:start_date],
+      end_date: r[:end_date],
+      localized_description: r[:guest_details][:localized_description],
+      nights: r[:nights],
+      number_of_adults: r[:guest_details][:number_of_adults],
+      number_of_children: r[:guest_details][:number_of_children],
+      number_of_infants: r[:guest_details][:number_of_infants],
+      number_of_guests: r[:number_of_guests],
+      status: r[:status_type],
+      currency: r[:host_currency],
+      payout_price: r[:expected_payout_amount],
+      security_price: r[:listing_security_price_accurate],
+      total_price: r[:expected_payout_amount]
+    )
+
+    guest
+  end 
+end
